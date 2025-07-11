088729088fd63ab677ecdd2725324c18183bae17
SpyMan <dev.spyman@gmail.com>
2025-07-12 00:55:20 +0200

    initial

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r ./hypr     ~/.config
+cp -r ./kitty    ~/.config
+cp -r ./ohmyposh ~/.config
+cp -r ./rofi     ~/.config
---
+++ fetch.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r ~/.config/hypr     ./
+cp -r ~/.config/kitty    ./
+cp -r ~/.config/ohmyposh ./
+cp -r ~/.config/rofi     ./
