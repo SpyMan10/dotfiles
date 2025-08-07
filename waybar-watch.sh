0a1085537c23e8a9c090884b407d5f00568c5817
SpyMan <dev.spyman@gmail.com>
2025-08-07 20:18:06 +0200

    feat: waybar reworked

---
+++ waybar-watch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CONFIG_LOC="/home/spyman/.config/waybar/config.jsonc"
+STYLE_LOC="/home/spyman/.config/waybar/style.css"
+
+echo "waybar-watch.sh: watching $CONFIG_LOC, $STYLE_LOC ..."
+
+trap "killall waybar" EXIT
+
+while true; do
+  waybar & 
+  echo "waybar-watch.sh: reloaded waybar"
+  inotifywait -e create,modify $CONFIG_LOC $STYLE_LOC
+  killall waybar
+done
